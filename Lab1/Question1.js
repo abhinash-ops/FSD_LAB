420581546d465fa72480143efe080dd3fe0b3376
abhinash-ops <abhinashgunreddy@gmail.com>
2025-01-07 18:35:50 +0530

    Initial commit for Q1

---
+++ Lab1/Question1.js
@@ -0,0 +1,15 @@
+//let variable
+console.log(a)
+let a = 10
+console.log(a)
+
+//var variable
+console.log(b)
+var b = 20
+
+console.log(b)
+
+//const variable
+console.log(c)
+const c = 30
+console.log(c)
